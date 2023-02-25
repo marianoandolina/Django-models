b0b1747d9bf8615ddc007b4b87cc5b1eadd61fa7
Mariano Andolina <mariano.3gs@gmail.com>
2023-02-25 11:04:30 -0300

    1st commit

--- marvel/e_commerce/models.py
+++ marvel/e_commerce/models.py
@@ -12,19 +12,25 @@ class Comic(models.Model):
     e_commerce_comic. Las columnas toman el nombre especificado de cada objeto.
     '''
     id = models.BigAutoField(db_column='ID', primary_key=True)
+
     marvel_id = models.PositiveIntegerField(
         verbose_name='marvel id', null=False, blank=False, unique=True
     )
+
     title = models.CharField(
         verbose_name='title', max_length=120, default=''
     )
+
     description = models.TextField(verbose_name='description', default='')
+
     price = models.FloatField(
         verbose_name='price', max_length=5, default=0.00
     )
+
     stock_qty = models.PositiveIntegerField(
         verbose_name='stock qty', default=0
     )
+
     picture = models.URLField(verbose_name='picture', default='')
 
     class Meta:
@@ -40,4 +46,30 @@ class Comic(models.Model):
         El método __str__ cumple una función parecida a __repr__ en SQL Alchemy, 
         es lo que retorna cuando llamamos al objeto.
         '''
-        return f'{self.id}'
+        return f'{self.title} - {self.id}'
+
+class WishList(models.Model):
+    id = models.BigAutoField(db_column=ID, primary_key=True)
+
+    user = models.ForeignKey(user, null=False, blank=False, on_delete=models.CASCADE)
+
+    comic = models.ForeignKey(Comic, verbose_name='Comic', default=1, blank=True, on_delete=models.CASCADE)
+
+    favorite = models.BooleanField(null=True, blank=True)
+
+    cart = models.PositiveIntegerField(null=True, blank=True)
+
+    wished_qty = models.PositiveIntegerField(null=True, blank=True)
+
+    bought_qty = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'whish_list'
+        verbose_name = 'whish_list'
+
+    def __str__(self):
+        return f'{self.user} - {self.comic}'    
+        
+
+
+
